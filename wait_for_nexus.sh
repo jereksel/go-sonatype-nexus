4faa7391fcab2cc81e4d312e0f94d8731bc6da68
Andrzej Ressel <jereksel@gmail.com>
2019-01-22 19:01:35 +0100

    Add drone.io

--- wait_for_nexus.sh
+++ wait_for_nexus.sh
@@ -7,7 +7,7 @@ echo "HOSTNAME: $HOSTNAME"
 for i in $(seq 1 300); do
     STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" -L "http://${HOSTNAME}:8081/")
 
-    echo "${i}: ${STATUS_CODE}"
+    # echo "${i}: ${STATUS_CODE}"
 
     if [ "${STATUS_CODE}" -eq "200" ]; then
         exit 0
